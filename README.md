47d311cd08201af9da65614644657b2c8a2791d2
wickedOne <wicliff.wolda@gmail.com>
2021-10-02 11:49:55 +0200

    phpstorm reporter

---
+++ README.md
@@ -0,0 +1,28 @@
+# PHP CodeSniffer PhpStorm reporter
+
+prints additional PhpStorm editor url in PHPCS cli output.
+
+## installation
+```bash
+$ composer require --dev wickedone/phpcs-reporter
+```
+
+### command line usage:
+specify this report on the command line:
+
+```bash
+$ php vendor/bin/phpcs --report='WickedOne\PHPCSReport\PhpStormReport'
+```
+
+### phpcs xml configuration
+specify this report in your ``phpcs.xml.dist``:
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+
+<ruleset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:noNamespaceSchemaLocation="vendor/squizlabs/php_codesniffer/phpcs.xsd">
+
+    <arg name="report" value="WickedOne\PHPCSReport\PhpStormReport" />
+    ...
+
+```
---
+++ src/PhpStormReport.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of WickedOne\PHPCSReporter.
+ *
+ * (c) wicliff <wicliff.wolda@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WickedOne\PHPCSReport;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Reports\Full;
+
+/**
+ * PhpStorm Report
+ *
+ * @author wicliff <wicliff.wolda@gmail.com>
+ */
+class PhpStormReport extends Full
+{
+    /**
+     * {@inheritdoc
+     */
+    public function generateFileReport($report, File $phpcsFile, $showSources = false, $width = 80): bool
+    {
+        if ($report['errors'] === 0 && $report['warnings'] === 0) {
+            return false;
+        }
+
+        parent::generateFileReport($report, $phpcsFile, $showSources, $width);
+
+        if (count($report['messages']) > 1) {
+            echo sprintf("✏️  phpstorm://open?file=%s", $phpcsFile->path).PHP_EOL;
+        } else {
+            echo sprintf("✏️  phpstorm://open?file=%s&line=%d", $phpcsFile->path, key($report['messages'])).PHP_EOL;
+        }
+
+        return true;
+    }
+}
