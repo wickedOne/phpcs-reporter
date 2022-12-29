9273c7d878e335da3f9608bef41d1d93e5ee8f8b
wickedOne <wicliff.wolda@gmail.com>
2022-12-29 05:50:52 +0100

    php-cs-fixer

--- src/PhpStormReport.php
+++ src/PhpStormReport.php
@@ -17,27 +17,27 @@ use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Reports\Full;
 
 /**
- * PhpStorm Report
+ * PhpStorm Report.
  *
  * @author wicliff <wicliff.wolda@gmail.com>
  */
 class PhpStormReport extends Full
 {
     /**
-     * {@inheritdoc
+     * {@inheritdoc.
      */
     public function generateFileReport($report, File $phpcsFile, $showSources = false, $width = 80): bool
     {
-        if ($report['errors'] === 0 && $report['warnings'] === 0) {
+        if (0 === $report['errors'] && 0 === $report['warnings']) {
             return false;
         }
 
         parent::generateFileReport($report, $phpcsFile, $showSources, $width);
 
-        if (count($report['messages']) > 1) {
-            echo sprintf("✏️  phpstorm://open?file=%s", $phpcsFile->path).PHP_EOL;
+        if (\count($report['messages']) > 1) {
+            echo sprintf('✏️  phpstorm://open?file=%s', $phpcsFile->path).\PHP_EOL;
         } else {
-            echo sprintf("✏️  phpstorm://open?file=%s&line=%d", $phpcsFile->path, key($report['messages'])).PHP_EOL;
+            echo sprintf('✏️  phpstorm://open?file=%s&line=%d', $phpcsFile->path, key($report['messages'])).\PHP_EOL;
         }
 
         return true;
